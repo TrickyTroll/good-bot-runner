1e4ed557dad36cabed6d14e00cc9e5ef25c14c3e
TrickyTroll <tricky@hey.com>
2021-05-08 17:58:23 -0400

    Add: Better documentation in funcmodule

--- runner/funcmodule.py
+++ runner/funcmodule.py
@@ -1,3 +1,19 @@
+# -*- coding: utf-8 -*-
+"""Functions used by the ``runner`` program.
+
+This module contains the function used by ``runner`` to parse
+configuration files.
+
+Example:
+    To parse a config, you can simply::
+
+        import funcmodule
+        parsed = funcmodule.parse_config(conf)
+    
+    ``parsed`` will be a ``dict`` representation of a ``yaml``
+    text file.
+"""
+
 import sys
 import yaml
 from io import TextIOWrapper
@@ -10,21 +26,30 @@ def parse_config(conf: TextIOWrapper) -> dict:
     Not to be used on the main conf file.
 
     Args:
-        conf (TextIOWrapper): The opened text file
+        conf (TextIOWrapper): The opened text file. This should
+        be created by the 
+        `click <https://click.palletsprojects.com/en/7.x/>`
+        library.
 
     Returns:
-        dict: A dict that contains info on the command.
+        dict: A dict that contains info on the command. The
+        keys will either be ``commands`` or ``expect``. Values
+        should be ``lists`` of shell commands or stuff to
+        expect before running those shell commands.
     """
     parsed = yaml.safe_load(conf)
 
     if type(parsed) != dict:
-        print("Wrong type of config file")
+        print("Wrong type of config file.")
         sys.exit()
 
     return parsed
 
 
-# For debugging
+#######################################################################
+#                             Debugging                               #
+#######################################################################
+
 if __name__ == "__main__":
     import os
 
