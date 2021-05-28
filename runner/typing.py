3807d279e8436afe0f49ee5cdbeced663e4716ae
Étienne Parent <etienne.parent@beon.ca>
2021-05-28 01:14:41 +0000

    In progress: randomizing typing

---
+++ runner/typing.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""Functions to help with fake typing on the command line."""
+
+def is_typo(previous: str, to_type: str) -> bool:
+    """Determines wether or not a 
+
+    Args:
+        previous (str): [description]
+        to_type (str): [description]
+
+    Returns:
+        bool: [description]
+    """
