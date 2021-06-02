304b6d540d98cde293d34ca02ad9ad73182dba06
TrickyTroll <53831454+TrickyTroll@users.noreply.github.com>
2021-06-02 16:12:22 -0400

    Fix: tests according to new parser

--- tests/test_funcs.py
+++ tests/test_funcs.py
@@ -10,16 +10,14 @@ class TestParsing(unittest.TestCase):
     def test_returns_dict(self):
         """Testing that the function returns a `dict`."""
 
-        with open(CONFIGPATH / "test_conf.yaml", "r") as config:
-            result = funcmodule.parse_config(config)
+        result = funcmodule.parse_config(CONFIGPATH / "test_conf.yaml")
 
         self.assertEqual(type(result), type({}))
 
     def test_return_format(self):
         """Tests for the correct return format."""
 
-        with open(CONFIGPATH / "test_conf.yaml", "r") as config:
-            result = funcmodule.parse_config(config)
+        result = funcmodule.parse_config(CONFIGPATH / "test_conf.yaml")
 
         for keys, values in result.items():
             self.assertEqual(type(result), type({}))
